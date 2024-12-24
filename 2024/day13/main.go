ab25945da6a7ca65897cf72e60ec7007e013dd3c
gautamdurian <gautam.chawla@durian.money>
2024-12-24 16:24:59 +0700

    cleanup

--- 2024/day13/main.go
+++ 2024/day13/main.go
@@ -25,15 +25,12 @@ func GetInputsFromFile() [][]string {
 		dataSets = append(dataSets, set)
 	}
 
-	fmt.Println(dataSets)
-
 	return dataSets
 }
 
 func splitDetails(inp string, separator string) (result []int) {
 	aSplit := strings.Split(inp, ": ")
 	aSplitXY := strings.Split(aSplit[1], ", ")
-	fmt.Println("xy split", aSplitXY)
 	aX := strings.Split(aSplitXY[0], separator)
 	aXInt, err := strconv.Atoi(aX[1])
 	if err != nil {
@@ -57,12 +54,36 @@ func getSetDetails(inp []string) (a []int, b []int, prize []int) {
 
 	prize = splitDetails(inp[2], "=")
 
-	fmt.Println("a->", a, "b->", b, "prize->", prize)
-
 	return
 }
 
 func SolveP1(inp [][]string) int {
+	var answer int
+
+	for _, i := range inp {
+		a, b, prize := getSetDetails(i)
+		ax := a[0]
+		ay := a[1]
+		bx := b[0]
+		by := b[1]
+		px := prize[0]
+		py := prize[1]
+
+		var na, nb int
+
+		if ((px*ay)-(py*ax))%((ay*bx)-(by*ax)) == 0 {
+			nb = ((px * ay) - (py * ax)) / ((ay * bx) - (by * ax))
+			if ((px - (bx * nb)) % ax) == 0 {
+				na = (px - (bx * nb)) / ax
+				answer += na*3 + nb
+			}
+		}
+	}
+
+	return answer
+}
+
+func SolveP2(inp [][]string) int {
 	var answer int
 
 	for _, i := range inp {
@@ -80,7 +101,6 @@ func SolveP1(inp [][]string) int {
 			nb = ((px * ay) - (py * ax)) / ((ay * bx) - (by * ax))
 			if ((px - (bx * nb)) % ax) == 0 {
 				na = (px - (bx * nb)) / ax
-				fmt.Println("na->", na, "nb", nb)
 				answer += na*3 + nb
 			}
 		}
@@ -89,16 +109,9 @@ func SolveP1(inp [][]string) int {
 	return answer
 }
 
-func SolveP2() int {
-	var answer int
-
-	return answer
-
-}
-
 func main() {
 	fmt.Println("Hello")
 	data := GetInputsFromFile()
 	fmt.Println("p1 -> ", SolveP1(data))
-	// fmt.Println("p2 -> ", SolveP2(data))
+	fmt.Println("p2 -> ", SolveP2(data))
 }
